9d3c195f78e5147d2dccb5c3c74f2ff7ada11932
joshuayabut <yabut.joshua@gmail.com>
2017-04-21 21:44:50 -0400

    Initial commit

---
+++ build-mac.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#example: ./build-win.sh -e ../eleos/ -d ../eleos-daemons/v0.0.4/ -v 0.0.4
+
+set -e
+
+daemonDir=''
+eleosDir=''
+version=''
+
+scriptDir=`pwd`
+
+while getopts 'd:e:v:' flag; do
+  case "${flag}" in
+    d) daemonDir="${OPTARG}" ;;
+    e) eleosDir="${OPTARG}" ;;
+    v) version="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+# prep npm package
+cd $eleosDir;
+npm install
+$(npm bin)/electron-rebuild
+
+# clean up eleos directory
+rm -f eleos.htpasswd
+rm -f *exe zcld* zcl-*
+rm -f *exe zcashd* zcash-*
+
+# copy MacOS daemons
+cp $daemonDir/zcld-mac ./
+cp $daemonDir/zcashd-mac ./
+cp $daemonDir/zcl-cli ./
+cp $daemonDir/zcash-cli ./
+
+# build .app
+cd ../
+electron-packager ./eleos eleos --app-version=${version} --icon=${scriptDir}/zclassic.icns --overwrite --osx-sign="Joshua Yabut (WWCFLJ762K)"
+
+# fix dylibs
+dylibbundler -od -b -x ./eleos-darwin-x64/eleos.app/Contents/Resources/app/zcashd-mac -x ./eleos-darwin-x64/eleos.app/Contents/Resources/app/zcash-cli -x ./eleos-darwin-x64/eleos.app/Contents/Resources/app/zcld-mac -x ./eleos-darwin-x64/eleos.app/Contents/Resources/app/zcl-cli -d ./eleos-darwin-x64/eleos.app/Contents/libs/ -p @executable_path/../../libs/
+
+# codesign executables
+cd ./eleos-darwin-x64/eleos.app/Contents/Resources/app/
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" zcld-mac
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" zcl-cli
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" zcashd-mac
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" zcash-cli
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" ../../libs/libgcc_s.1.dylib
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" ../../libs/libgomp.1.dylib
+codesign --force --sign "Developer ID Application: Joshua Yabut (WWCFLJ762K)" ../../libs/libstdc++.6.dylib
+
+# codesign app
+cd ${scriptDir}/../
+codesign --force --deep --sign "Joshua Yabut (WWCFLJ762K)" eleos-darwin-x64/eleos.app/
+
+# build .dmg and codesign
+electron-installer-dmg ${scriptDir}/../eleos-darwin-x64/eleos.app/ eleos-macos-v${version} --background=${scriptDir}/eleos_bg.png --overwrite
+codesign --force --sign "Joshua Yabut (WWCFLJ762K)" eleos-macos-v${version}.dmg
---
+++ build-win.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#example: ./build-win.sh -e ../eleos/ -d ../eleos-daemons/v0.0.4/ -v 0.0.4
+
+set -e
+
+daemonDir=''
+eleosDir=''
+version=''
+
+scriptDir=`pwd`
+
+while getopts 'd:e:v:' flag; do
+  case "${flag}" in
+    d) daemonDir="${OPTARG}" ;;
+    e) eleosDir="${OPTARG}" ;;
+    v) version="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+# prep npm package
+cd $eleosDir;
+npm install
+$(npm bin)/electron-rebuild
+
+# clean up eleos directory
+rm -f eleos.htpasswd
+rm -f *exe zcld* zcl-*
+rm -f *exe zcashd* zcash-*
+
+# copy Win64 daemons
+cp $daemonDir/zcash-cli.exe ./
+cp $daemonDir/zcashd.exe ./
+cp $daemonDir/zcl-cli.exe ./
+cp $daemonDir/zcld.exe ./
+
+# build .app
+cd ../
+electron-packager ./eleos eleos --app-version=${version} --arch=x64 --platform=win32 --icon=./eleos-build/zcl.ico --overwrite
